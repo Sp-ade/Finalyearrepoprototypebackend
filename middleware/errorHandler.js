ec70fad47ebc954d33b9151017e2434c31ffbe05
Sp-ade <ademola1304@gmail.com>
2026-04-30 12:57:24 +0100

    added file being too large to error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,15 @@
 module.exports = function errorHandler(err, req, res, next) {
   console.error(err)
   if (res.headersSent) return next(err)
+
+  // Handle Multer errors specifically
+  if (err.code === 'LIMIT_FILE_SIZE') {
+    return res.status(400).json({ 
+      message: 'File too large. Maximum size allowed is 8MB.',
+      error: 'LIMIT_FILE_SIZE'
+    })
+  }
+
   const status = err.status || (err.name === 'UnauthorizedError' ? 401 : 500)
   res.status(status).json({ message: err.message || 'Internal Server Error' })
 }
