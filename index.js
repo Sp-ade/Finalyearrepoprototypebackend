25ecda0d95c70cba0839620460cfb1f6b1537464
ademo <ademola1304@gmail.com>
2026-07-13 21:37:57 +0100

    allowed patch command in CORS

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cors({
         'https://finalyearrepoprototypefrontend.onrender.com'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(helmet({
